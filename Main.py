24d6d3fead0583d2c590ca82511a45863957134b
Abdur Rahman <abdur.r10567@gmail.com>
2022-05-07 18:56:11 -0700

    Created main file and added method for hardcoded processes

---
+++ Main.py
@@ -0,0 +1,15 @@
+
+
+
+def hardCodedProcesses():
+    a = 1
+
+
+def main():
+    #create some hard coded processes.
+    listOfProcesses = hardCodedProcesses()
+
+
+# call the main function if it exists
+if __name__ == '__main__':
+    main()
